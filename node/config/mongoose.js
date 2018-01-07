6c4e8b53ec911dfa21640a0c7f2515946f3f3562
astider@home <astider@gmail.com>
2018-01-07 13:32:14 +0700

    edit dependencies, add graphql

--- node/config/mongoose.js
+++ node/config/mongoose.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
-import autoincrement from 'mongoose-auto-increment'
+import autoIncrement from 'mongoose-auto-increment'
 
 const schema = mongoose.schema
 
 const connectMongoDB = () => {
     const db = mongoose.connect('mongodb://localhost:27017/todo-quest')
-    autoIncrement.initialize(db)
+    // autoIncrement.initialize(db)
 
-    const todoSchema = new Schema({
-        detail: String,
-    })
+    // const todoSchema = new Schema({
+    //     detail: String,
+    // })
 
-    todoSchema.plugin(autoIncrement.plugin, { model: 'todo', field: 'todoId' })
+    // todoSchema.plugin(autoIncrement.plugin, { model: 'todo', field: 'todoId' })
 
     return db
 }
