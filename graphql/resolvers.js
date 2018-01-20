30a1505abfbe69900401cb39d161655a9c27ca55
astider@home <astider@gmail.com>
2018-01-20 21:54:07 +0700

    modify request function to support GET, POST, DELETE in one

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,17 @@
 const fetch = require('isomorphic-fetch')
 
-const request = (path, query) => {
-  return fetch(`http://localhost:8000${path}`)
+const request = (path, options) => {
+
+  const fetchOptions = {
+    method: options.method
+  }
+
+  if (options.method == 'POST' || options.method == 'DELETE') {
+    fetchOptions.body = JSON.stringify(options.data)
+    fetchOptions.headers = { 'Content-Type': 'application/json' }
+  }
+
+  return fetch(`http://localhost:8000${path}`, fetchOptions)
   .then(response => {
     if (response.status >= 400) {
       throw new Error("Bad response from server");
@@ -10,57 +20,25 @@ const request = (path, query) => {
   })
 }
 
-const post = (path, data) => {
-
-  console.log(`send POST to http://localhost:8000${path}`)
-
-  return fetch(`http://localhost:8000${path}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => {
-    if (response.status >= 400) {
-      console.log('return status : ' + response.status)
-      throw new Error("Bad response from server");
-    }
-    return response.json()
-  })
-}
-
-const del = (path, data) => {
-  return fetch(`http://localhost:8000${path}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => {
-    if (response.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return response.json()
-  })
-}
 
 module.exports = {
   Query: {
     listTodo() {
-      return request('/')
+      return request('/', { method: 'GET'} )
     }
   },
   Mutation: {
     createTodo(doc, { text }) {
-      return post('/', { inputText: text })
+      const options = { method: 'POST', data: { inputText: text } }
+      return request('/', options)
     },
     deleteTodo(doc, { todoId }) {
-      return del(`/${todoId}` , { todoId: todoId })
+      const options = { method: 'DELETE', data: { todoId: todoId } }
+      return request(`/${todoId}`, options)
     },
     toggleTodo(doc, { todoId }) {
-      return post(`/${todoId}/toggle` , { todoId: todoId })
+      const options = { method: 'POST', data: { todoId: todoId } }
+      return request(`/${todoId}/toggle`, options)
     }
   },
 }
